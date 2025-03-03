228f0a8fb0701e861435d9a8dc7b5e480544ab92
Pallab07-Code <paulpallab311@gmail.com>
2025-03-03 21:08:52 +0600

    Add web browsing module so that bot can execute user command using browser

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
+import webbrowser
+
 
 engine = pyttsx3.init()
 
@@ -7,9 +9,29 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def CommandProcessing(word):
+    if "open google" in word.lower():
+        webbrowser.open_new_tab("https://www.google.com")
+        speak("  opening google ")
+    elif "open youtube" in word.lower():
+        webbrowser.open_new_tab("https://www.youtube.com")
+        speak("  opening youtube ")
+    elif "open facebook" in word.lower():
+        webbrowser.open_new_tab("https://www.facebook.com")
+        speak("  opening facebook ")
+    elif "open instagram" in word.lower():
+        webbrowser.open_new_tab("https://www.instagram.com")
+        speak("  opening instagram ")
+    elif "open maps" in word.lower():
+        webbrowser.open_new_tab("https://www.google.com/maps")
+        speak("  opening maps ")
+    else:
+        speak(" Sorry sir i did not understand your command")
+
+
 if __name__ == "__main__":
     speak("Initializing shadow")
-    #Listen when someone called jarvis
+    #Listen when someone called shadow
 
     while True:
 
@@ -22,6 +44,18 @@ if __name__ == "__main__":
             print("processing your words:")
             command = r.recognize_google(audio)
             print(command)
+
+            #Wake up call
+            if(command.lower() == "shadow"):
+                speak("In your service sir")
+                with sr.Microphone() as source:
+                    print("Service mode activated")
+                    audio2 = r.listen(source,timeout=2)
+                    command2 = r.recognize_google(audio2)
+                    print(command)
+
+                    CommandProcessing(command2.lower())
+
 
             #for terminating shadow
             if(command == "ok bye"):
